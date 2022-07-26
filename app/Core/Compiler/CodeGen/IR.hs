67bfa14ca5c34187e2828803a0d34deff1a28a65
thomasvergne <thomas.vergne.contact@gmail.com>
2022-07-26 12:11:20 +0200

    ✨ Added isStatement function.

--- app/Core/Compiler/CodeGen/IR.hs
+++ app/Core/Compiler/CodeGen/IR.hs
@@ -1,12 +1,41 @@
 module Core.Compiler.CodeGen.IR where
+  import Core.TypeChecking.Type.AST (Literal)
   type CType = String
-  data CppAST 
+  data CppAST
     = CDeclaration (String, CType) CppAST
     | CModification CppAST CppAST
-    | CIf CppAST CppAST CppAST
     | CSequence [CppAST]
     | CReturn CppAST
-    | CStruct String [(String, CType)]
+    | CStruct String [StructField]
     | CTemplate [CType] CppAST
     | CEnum String [String]
+    | CIf CppAST CppAST
+    | CIfElse CppAST CppAST CppAST
+
+    | CCall CppAST [CppAST] [CType]
+    | CLamStruct CType [(String, CppAST)]
+    | CRef CppAST
+    | CDeref CppAST
+    | CLambda [String] [(String, CType)] CppAST
+    | CVariable String
+    | CBinCall CppAST String CppAST
+    | CStructProp CppAST String
+    | Lit Literal
     deriving Show
+
+  data StructField
+    = SType String CType
+    | SLambda CType String [(String, CType)] CppAST
+    deriving Show
+
+  isStatement :: CppAST -> Bool
+  isStatement (CDeclaration _ _) = True
+  isStatement (CModification _ _) = True
+  isStatement (CSequence _) = True
+  isStatement (CReturn _) = True
+  isStatement (CStruct _ _) = True
+  isStatement (CTemplate _ _) = True
+  isStatement (CEnum _ _) = True
+  isStatement (CIf _ _) = True
+  isStatement CIfElse {} = True
+  isStatement _ = False
