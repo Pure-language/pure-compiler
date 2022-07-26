0af18dfd6842337f0ad7b050ca58e358df9f8bc5
thomasvergne <thomas.vergne.contact@gmail.com>
2022-07-26 12:11:05 +0200

    ♻️ Moved some type functions to type module.

--- app/Core/Compiler/Compiler.hs
+++ app/Core/Compiler/Compiler.hs
@@ -5,63 +5,77 @@ module Core.Compiler.Compiler where
   import Data.List (intercalate, union)
   import Core.TypeChecking.Type.Definition
   import Core.TypeChecking.Type.AST
-  import Core.Compiler.CodeGen (CppAST(..))
+  import Core.Compiler.CodeGen (CppAST(..), CType)
+  import Control.Monad.RWS (gets)
+  import Control.Monad.State (StateT(runStateT), evalStateT)
+  import Core.Compiler.Modules.ADT (createEnum, createStructure)
+  import Core.Compiler.Modules.Pattern (compileCase)
   
-  createTypeMap :: Type -> [String]
-  createTypeMap Int = []
-  createTypeMap Bool  = []
-  createTypeMap Char  = []
-  createTypeMap String  = []
-  createTypeMap Float  = []
-  createTypeMap (t1 :-> t2) =  t1' `union` createTypeMap t2
-    where t1' = foldl union [] $ map createTypeMap t1
-  createTypeMap (TVar x) = ["A" ++ show x]
-  createTypeMap (ListT t) = createTypeMap t
-  createTypeMap (TRec t) = foldl union [] m
-    where f = map snd t
-          m = map createTypeMap f
-  createTypeMap (TApp n t) = foldl union [] $ map createTypeMap t
-  createTypeMap (RefT t) = createTypeMap t
-
-  fromType :: Type -> String
-  fromType Int = "int"
-  fromType Bool  = "bool"
-  fromType Char  = "char"
-  fromType String  = "char*"
-  fromType Float  = "float"
-  fromType (TVar x) = "A" ++ show x
-  fromType (ListT t) = fromType t ++ "*"
-  fromType (TRec t) = "struct {" ++  f ++ "}"
-    where f = intercalate ", " $ map (\(x,t) -> x ++ ": " ++ fromType t) t
-  fromType (TApp n t) = n ++ "<" ++ intercalate "," (map fromType t) ++ ">"
-  fromType (t1 :-> t2) = "std::function<" ++ fromType t2 ++ "(" ++ intercalate "," (map fromType t1) ++ ")" ++ ">"
-  fromType (RefT t) = fromType t ++ "*"
-
-  createTemplate :: Type -> String
-  createTemplate t = "template<" ++ intercalate "," m ++ ">"
-    where m = createTypeMap t
-
   compileStatement :: MonadCompiler m => TypedStatement -> m [CppAST]
   compileStatement (Assignment (name :@ ty) e) = do
     e' <- compileExpression e
-    return $ [CTemplate (createTypeMap ty) $ CDeclaration (name, fromType ty) e']
+    return [CTemplate (createTypeMap ty) $ CDeclaration (name, fromType ty) e']
   compileStatement (Modified n e) = do
     n' <- compileExpression n
     e' <- compileExpression e
-    return $ [CModification n' e']
+    return [CModification n' e']
   compileStatement (If e s1 s2) = do
     e' <- compileExpression e
     s1' <- compileStatement s1
     s2' <- compileStatement s2
-    return $ [CIf e' (CSequence s1') (CSequence s2')]
+    return [CIfElse e' (CSequence s1') (CSequence s2')]
   compileStatement (Sequence stmts) = do
     stmts' <- mapM compileStatement stmts
     return $ concat stmts'
-  compileStatement (Expression e) 
+  compileStatement (Expression e)
     = (:[]) <$> compileExpression e
   compileStatement (Return e)
     = (:[]) . CReturn <$> compileExpression e
-  compileStatement (Enum (n, ty) fields) = undefined
+  compileStatement z@(Enum (n, ty) fields) = (:[createEnum z]) <$> createStructure z
 
   compileExpression :: MonadCompiler m => TypedExpression -> m CppAST
-  compileExpression _ = undefined
+  compileExpression (Variable name) = return $ CVariable name
+  compileExpression (FunctionCall call args ty) = do
+    call' <- compileExpression call
+    args' <- mapM compileExpression args
+    return $ CCall call' args' []
+  compileExpression (Reference c) = CRef <$> compileExpression c
+  compileExpression (Unreference c) = CDeref <$> compileExpression c
+  compileExpression z@(Lambda args body) = do
+    let env = free z
+    let args' = map (\(x :@ t) -> (x, fromType t)) args
+    body' <- compileStatement body
+    return $ CLambda env args' $ CSequence body'
+  compileExpression (Match x pats) = do
+    x <- compileExpression x
+    CSequence <$> mapM (\(p, b) -> do
+      b <- compileStatement b
+      p <- compileCase p
+      return $ p x b) pats
+  compileExpression (Constructor c) = do
+    getEnv >>= \env -> case M.lookup c env of
+      Just (e, i) -> return $ if i then CCall (CStructProp (CVariable e) c) [] [] else CStructProp (CVariable e) c
+      _ -> error $ "Constructor " ++ c ++ " is not an enum"
+  compileExpression x = error . show $ x
+
+  match :: [Type] -> [Type] -> [CType]
+  match (t:ts) (t':ts') = matchType t t' ++ match ts ts'
+  match _ _ = []
+
+  matchType :: Type -> Type -> [CType]
+  matchType Int Int = ["int"]
+  matchType Bool Bool = ["bool"]
+  matchType Char Char = ["char"]
+  matchType String String = ["char*"]
+  matchType Float Float = ["float"]
+  matchType (TVar u) t = [fromType t]
+  matchType (t1 :-> t2) (t1' :-> t2') = t' `union` matchType t2 t2'
+    where t' = foldl union [] $ zipWith matchType t1 t1'
+  matchType (TApp _ t) (TApp _ t') = foldl union [] $ zipWith matchType t t'
+  matchType (TRec t) (TRec t') = foldl union [] $ zipWith matchType (map snd t) (map snd t')
+  matchType (RefT t) (RefT t') = matchType t t'
+  matchType _ _ = []
+
+  runCompiler :: Monad m => [TypedStatement] -> m CppAST
+  runCompiler stmts =
+    CSequence . concat <$> evalStateT (mapM compileStatement stmts) emptyState
--- app/Core/Compiler/Type.hs
+++ app/Core/Compiler/Type.hs
@@ -1,3 +1,39 @@
-module Core.Compiler.Type (module Type) where
+module Core.Compiler.Type (module Type, createTemplate, createTypeMap, fromType) where
   import Core.Compiler.Type.Free as Type
   import Core.Compiler.Type.Monad as Type
+  import Core.TypeChecking.Type.Definition (Type(..))
+  import Data.List (union, intercalate)
+  
+  createTypeMap :: Type -> [String]
+  createTypeMap Int = []
+  createTypeMap Bool  = []
+  createTypeMap Char  = []
+  createTypeMap String  = []
+  createTypeMap Float  = []
+  createTypeMap (t1 :-> t2) =  t1' `union` createTypeMap t2
+    where t1' = foldl union [] $ map createTypeMap t1
+  createTypeMap (TVar x) = ["A" ++ show x]
+  createTypeMap (ListT t) = createTypeMap t
+  createTypeMap (TRec t) = foldl union [] m
+    where f = map snd t
+          m = map createTypeMap f
+  createTypeMap (TApp n t) = foldl union [] $ map createTypeMap t
+  createTypeMap (RefT t) = createTypeMap t
+
+  fromType :: Type -> String
+  fromType Int = "int"
+  fromType Bool  = "bool"
+  fromType Char  = "char"
+  fromType String  = "char*"
+  fromType Float  = "float"
+  fromType (TVar x) = "A" ++ show x
+  fromType (ListT t) = fromType t ++ "*"
+  fromType (TRec t) = "struct {" ++  f ++ "}"
+    where f = intercalate ", " $ map (\(x,t) -> x ++ ": " ++ fromType t) t
+  fromType (TApp n t) = n ++ "<" ++ intercalate "," (map fromType t) ++ ">"
+  fromType (t1 :-> t2) = "std::function<" ++ fromType t2 ++ "(" ++ intercalate "," (map fromType t1) ++ ")" ++ ">"
+  fromType (RefT t) = fromType t ++ "*"
+
+  createTemplate :: Type -> String
+  createTemplate t = "template<" ++ intercalate "," m ++ ">"
+    where m = createTypeMap t
