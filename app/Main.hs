3c17e31dfd4d8b9a0a2bd960420956ab28be0f81
thomasvergne <thomas.vergne.contact@gmail.com>
2022-09-17 21:36:47 +0200

    ⚙️ Added passes calls in main file.

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,15 @@ module Main where
   import Data.Void (Void)
   import Core.TypeChecking.Type (runCheck)
   import Text.Parsec.Pos
+  import Core.Compiler.Compiler (runCompiler)
+  import Core.Conversion.Close (runClosureConversion)
+  import Core.Conversion.Hoisting (runHoisting)
+  import Core.ANF (runANF)
+  import Core.Monomorphization (monomorphize)
+  import Core.Compiler.CodeGen.Generation (from)
+  import Core.Compiler.Type (CompilerState(toplevel))
+  import Core.Isolation (isolateToplevel)
+  import Data.Bifunctor (Bifunctor(bimap))
   
   instance HasHints Void [Char] where
     hints _ = mempty
@@ -20,7 +29,19 @@ module Main where
       Right ast -> do
         ast' <- runCheck ast
         case ast' of
-          Right ast -> print ast
+          Right ast -> do
+            ast <- monomorphize ast
+            (structs, ast) <- runClosureConversion ast
+            ast <- runHoisting ast
+            let (enums, ast') = bimap concat concat $ unzip $ map isolateToplevel ast
+            mapM_ ((>> putStrLn "") . print) ast'
+            ast <- runANF (enums ++ structs ++ ast')
+            mapM_ ((>> putStrLn "") . print) ast
+            (c, st) <- runCompiler ast
+            --mapM_ ((>> putStrLn "") . print) c
+            let tl = unlines $ reverse $ toplevel st
+            let c' = concatMap ((++";") . from) c
+            writeFile "test.c" (tl ++ c')
           Left (error, msg, (p1, p2)) -> do
             let p1' = (sourceLine p1, sourceColumn p1)
             let p2' = (sourceLine p2, sourceColumn p2)
