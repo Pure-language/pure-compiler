d0e2d6202d8e4203a4087d4ce1b49ee5d2daac49
thomasvergne <thomas.vergne.contact@gmail.com>
2022-10-12 21:21:39 +0200

    🔥 Removed monomorphization.

--- app/Core/Isolation.hs
+++
@@ -1,6 +0,0 @@
-module Core.Isolation where
-  import Core.TypeChecking.Type.AST (TypedStatement(..))
-
-  isolateToplevel :: TypedStatement -> ([TypedStatement], [TypedStatement])
-  isolateToplevel z@(Enum _ _) = ([z], [])
-  isolateToplevel z = ([], [z])
