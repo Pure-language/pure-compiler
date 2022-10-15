d450bf648e681c26f9b9475db261b34996636792
thomasvergne <thomas.vergne.contact@gmail.com>
2022-10-15 17:08:33 +0200

    👽 Updated ADT and pattern matching compilation.

--- app/Core/Compiler/Modules/ADT.hs
+++ app/Core/Compiler/Modules/ADT.hs
@@ -1,41 +1,22 @@
+{-# LANGUAGE LambdaCase #-}
 module Core.Compiler.Modules.ADT where
-  import Core.TypeChecking.Type.AST (TypedStatement(..), Annoted ((:@)))
-  import Core.Compiler.CodeGen.IR (CppAST(..), StructField (SType))
+  import Core.TypeChecking.Type.AST (TypedStatement(..), Annoted ((:@)), Literal (S))
+  import Core.Compiler.CodeGen.IR (IR(..), varify)
   import Core.Compiler.Type
   import Debug.Trace (traceShow, traceShowM)
-  import Core.TypeChecking.Type.Definition (Type(TApp, (:->)))
+  import Core.TypeChecking.Type.Definition (Type(TApp, (:->), TRec))
   import Control.Monad.RWS (modify)
-  
-  createEnum :: TypedStatement -> CppAST
-  createEnum (Enum (n, _) fields) = do
-    let fields' = map (\(n :@ _) -> n ++ "C") fields
-    CEnum (n ++ "E") fields'
-  createEnum _ = undefined
 
-  createStructure :: MonadCompiler  m => TypedStatement -> m [CppAST]
-  createStructure (Enum (name, ty) fields) = do
-    let t = createTemplate ty
-    traceShowM ("cons", name)
-    modify $ \s -> s { structures = (name, ty) : structures s }
-    
-    n' <- fromType ty
-    let values = concatMap (\(n :@ t) -> case t of
-                  t :-> _ -> zip (["v" ++ show i | i <- [0..]]) t
-                  _ -> []) fields
-    fields' <- mapM (\(n, t) -> SType n <$> fromType t) values
-    let enumField = SType "TYPE" $ "enum " ++ name ++ "E"
-    methods <- mapM (\(n :@ t) -> case t of
-        t :-> _ -> do
-          addEnv n True name
-          traceShowM n
-          t <- mapM fromType t
-          let args = zip (["v" ++ show i | i <- [0..]]) t
-          let args' = map fst args
-          let struct = CCast n' $ CLamStruct $ ("TYPE", CVariable (n ++ "C")) : zip args' (map CVariable args')
-          return $ CFunction n' n args (CReturn struct)
-        _ -> do
-          addEnv n False name
-          let struct = CCast n' $ CLamStruct [("TYPE", CVariable (n ++ "C"))]
-          return $ CFunction n' n [] (CReturn struct)) fields
-    return $ CStruct name (enumField : fields') : methods
-  createStructure _ = undefined
+  compileData :: MonadCompiler m => TypedStatement -> m IR
+  compileData (Enum (n, _) cons) = do
+    cons' <- mapM (\(n' :@ t) -> do
+              addConstructor (varify n') (varify n)
+              case t of
+                args :-> _ -> do
+                  let args' = zipWith const (["v" ++ show i | i <- [0..]]) args
+                  let fields = map (\x -> (x, IRVariable x)) args'
+                  let fields' = IRLamStruct (("type", IRLit (S n')) : fields)
+                  return (varify n', IRLambda args' (IRReturn fields'))
+                _ -> return (varify n', IRLamStruct [("type", IRLit (S n'))])) cons
+    return $ IRDeclaration (varify n) (IRLamStruct cons')
+  compileData _ = error "compileData: not a data"
--- app/Core/Compiler/Modules/Pattern.hs
+++ app/Core/Compiler/Modules/Pattern.hs
@@ -1,53 +1,53 @@
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE LambdaCase #-}
 module Core.Compiler.Modules.Pattern where
   import Core.TypeChecking.Type.AST (TypedPattern(..), Literal(..))
-  import Core.Compiler.CodeGen.IR (CppAST(..), isStatement)
-  import Core.Compiler.Type (fromType, MonadCompiler, getEnv)
+  import Core.Compiler.CodeGen.IR (IR(..), varify)
+  import Core.Compiler.Type (MonadCompiler, getConstructor)
   import qualified Data.Map as M
   import Prelude hiding (and)
   import Data.Maybe (fromJust, isJust)
   import Control.Monad (zipWithM)
 
-  and :: CppAST -> CppAST -> CppAST
-  and l = CBinCall l "&&"
+  and :: IR -> IR -> IR
+  and l = IRBinCall l "&&"
 
-  findPattern :: MonadCompiler m => TypedPattern -> m (CppAST -> [(Maybe CppAST, Maybe CppAST)])
-  findPattern (LitP l)    = return $ \e -> [(Nothing, Just $ CBinCall e "==" (Lit l))]
-  findPattern WilP        = return $ const [(Nothing, Nothing)]
+  findPattern :: MonadCompiler m => TypedPattern -> m (IR -> [(Maybe IR, Maybe IR)])
+  findPattern (LitP l)    = 
+    return $ \e -> [(Nothing, Just $ IRBinCall e "==" (IRLit l))]
+  findPattern WilP        = 
+    return $ const [(Nothing, Nothing)]
   findPattern (VarP v t)  = do
-    t <- fromType t
-    return $ \e -> [(Just $ CDeclaration (v, t) e, Nothing)]
+    return $ \e -> [(Just $ IRDeclaration (varify v) e, Nothing)]
   findPattern (AppP n xs) = do
     xs' <- zipWithM (\x y -> (x,) <$> findPattern y) (["v" ++ show i | i <- [0..]]) xs
     return $ \e -> do
-      let xs'' = map (\(x, f) -> f (CStructProp e x)) xs'
-      concat ([(Nothing, Just $ CBinCall (CStructProp e "TYPE") "==" (CVariable $ n ++ "C"))] : xs'') 
+      let xs'' = map (\(x, f) -> f (IRStructProp e x)) xs'
+      concat ([(Nothing, Just $ IRBinCall (IRStructProp e "type") "===" (IRLit (S n)))] : xs'') 
 
-  compileCase :: MonadCompiler m => TypedPattern -> CppAST -> [CppAST] -> m CppAST
+  compileCase :: MonadCompiler m => TypedPattern -> IR -> [IR] -> m IR
   compileCase (VarP n t) = do
-    \x b -> getEnv >>= \e -> case M.lookup n e of
+    \x b -> getConstructor n >>= \case
       Just _ -> return $
-        let cond = CBinCall (CStructProp x "TYPE") "==" (CVariable $ n ++ "C")
-          in CIf cond $ CSequence b
-      Nothing -> do
-        t <- fromType t
-        return $  CSequence [CDeclaration (n, t) x, CSequence b]
+        let cond = IRBinCall (IRStructProp x "type") "===" (IRLit $ S n)
+          in IRIf cond $ IRSequence b
+      Nothing -> return $ IRSequence [IRDeclaration (varify n) x, IRSequence b]
   compileCase (AppP n args) = do
     let args' x = concat <$> zipWithM (\arg v -> do
               f <- findPattern arg
-              return $ f (CStructProp x v)) args (["v" ++ show i | i <- [0..]])
+              return $ f (IRStructProp x v)) args (["v" ++ show i | i <- [0..]])
     \x b -> do
       args_ <- args' x
       let lets   = map (fromJust . fst) $ filter (isJust . fst) args_
       let cs = map (fromJust . snd) $ filter (isJust . snd) args_
-      let cond  = CBinCall (CStructProp x "TYPE") "==" (CVariable $ n ++ "C")
+      let cond  = IRBinCall (IRStructProp x "type") "===" (IRLit $ S n)
           conds = case cs of
                     (c:cs) -> cond `and` foldl and c cs
                     _ -> cond
-        in return $ CIf conds $ CSequence $ lets ++ b
+        in return $ IRIf conds $ IRSequence $ lets ++ b
   compileCase WilP = do
-    \x b -> return $ CSequence b
+    \x b -> return $ IRSequence b
   compileCase (LitP l) = do
     \x b -> return $
-      let cond = CBinCall x "===" (Lit l)
-        in CIf cond $ CSequence b
+      let cond = IRBinCall x "===" (IRLit l)
+        in IRIf cond $ IRSequence b
